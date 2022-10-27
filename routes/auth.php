abaf020a6fe83f42835c49768e31d0dd591e19ba
resepngoding <resepngoding@gmail.com>
2022-10-27 22:10:20 +0700

    install and integrate socialite

--- routes/auth.php
+++ routes/auth.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
@@ -19,9 +20,7 @@ Route::middleware('guest')->group(function () {
     Route::get('login/admin', [AuthenticatedSessionController::class, 'create'])
         ->name('login.admin');
 
-    Route::get('login', function () {
-        return view('login');
-    })->name('login');
+    Route::get('login', [UserController::class, 'login'])->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
+
+//Tombol Google Sign ini di klik akan mengarah ke route ini:
+Route::get('/sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+//email google dipilih utnuk sign in akan mengarah ke route ini :
+Route::get('/auth/google/callback', [UserController::class, 'handleProvideCallback'])->name('user.google.callback');
 
 Route::get('/checkout', function () {
     return view('checkout');
